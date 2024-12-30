8836cd31dce95a9b125d60ac815db98d43a98c91
High-Gravity <flavorfulcode@yahoo.com>
2024-12-30 15:50:07 -0600

    Add list updating for 'Add' button

--- gui.py
+++ gui.py
@@ -2,11 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 
-label = sg.Text("Enter A To-Do: ")
-input_box = sg.InputText(tooltip="Enter Todo: ")
+label = sg.Text("Enter a To-Do: ")
+input_box = sg.InputText(tooltip="Enter Todo: ", key='todo')
 add_button = sg.Button("Add")
 
 
-window = sg.Window("ListLee", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("ListLee",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Georgia', 20))
+
+
+while True:
+   event,values = window.read()
+   match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
